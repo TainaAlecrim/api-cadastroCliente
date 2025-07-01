af477209bb7e9f2e5149ff48442e4a954e7e4c5e
Taina Alecrim <tai.estudos@gmail.com>
2025-07-01 12:51:41 -0300

    docker

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -3,7 +3,7 @@ namespace CadastroClientes.Api.Models
     public class Cliente
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Email { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,6 +10,17 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 // 2) Habilitar controllers:
 builder.Services.AddControllers();
 
+// ** Adicionar a configuração do CORS aqui **
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirFrontEnd", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")  // URL do seu front-end Blazor
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 // 3) (Opcional) Habilitar Swagger para documentação:
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +28,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // 4) Middleware:
+
+// Usar CORS logo após criar o app, antes de mapear controllers
+app.UseCors("PermitirFrontEnd");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
